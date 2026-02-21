91dc52bab7655d1c0f0435ff75bf0e767688b573
David Sheng <davidxsheng@gmail.com>
2026-02-21 16:28:57 -0500

    chore: remove admin panel

--- src/components/admin/AdminTabs.tsx
+++
@@ -1,9 +0,0 @@
-import { EntitiesManager } from './entities/EntitiesManager';
-
-export const AdminTabs = () => {
-  return (
-    <div className="flex w-full flex-col p-6">
-      <EntitiesManager />
-    </div>
-  );
-};
